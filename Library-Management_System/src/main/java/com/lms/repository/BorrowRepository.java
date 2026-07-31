69914433e98ad413cd48dcd5408228c58a657743
vennapusa prathyusha <vennapusaprathyusha1310@gmail.com>
2026-07-31 10:11:49 +0530

    Remove Library Management System project

--- Library-Management_System/src/main/java/com/lms/repository/BorrowRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lms.entity.Borrow;
-
-public interface BorrowRepository extends JpaRepository<Borrow,Integer > {
-
-}
